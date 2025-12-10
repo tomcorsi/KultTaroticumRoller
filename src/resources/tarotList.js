3e7a87df6d8dbac70c24fc6f18a5f81eed73a4c3
tomcorsi <106984720+tomcorsi@users.noreply.github.com>
2025-12-10 20:01:10 +0000

    feat: corresponding tarots added to list

--- src/resources/tarotList.js
+++ src/resources/tarotList.js
@@ -1,5 +1,10 @@
 import anthropos from './images/00-anthropos.jpg'
 import demiurgos from './images/01-demiurgos.jpg'
+import astaroth from './images/02-astaroth.jpg'
+import kether from './images/03-kether.jpg'
+import chokmah from './images/04-chokmah.jpg'
+import binah from './images/05-binah.jpg'
+import chesed from './images/06-chesed.jpg'
 
 export const majorArcana = [
     {
@@ -11,6 +16,31 @@ export const majorArcana = [
         id: 'Demiurgos',
         number: 1,
         imageSrc: demiurgos
+    },
+    {
+        id: 'Astaroth',
+        number: 2,
+        imageSrc: astaroth
+    },
+    {
+        id: 'Kether',
+        number: 3,
+        imageSrc: kether
+    },
+    {
+        id: 'Chokmah',
+        number: 4,
+        imageSrc: chokmah
+    },
+    {
+        id: 'Binah',
+        number: 5,
+        imageSrc: binah
+    },
+    {
+        id: 'Chesed',
+        number: 6,
+        imageSrc: chesed
     }
 ]
 export const minorArcana = []
