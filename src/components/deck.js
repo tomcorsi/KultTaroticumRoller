03f5dba6e43167454e8ae00147ccf80912a6cbeb
tomcorsi <106984720+tomcorsi@users.noreply.github.com>
2025-12-10 21:29:32 +0000

    feat: moved static deck on top left

--- src/components/deck.js
+++ src/components/deck.js
@@ -4,6 +4,9 @@ import { Card } from './basicCard';
 const StyledDeck = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
+  padding: 10px;
+  flex-shrink: 0;
 `;
 
 export const Deck = ({ tellFortune }) => {
